7b6272baa65b957cc0a35c30a4aae80df92523cf
Berkanktk <berkank@live.dk>
2022-07-04 13:35:44 +0200

    Added first version

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# random-person
+# Random Person Generator
+Made with the use of the randomuser.me API and vue.js CDN.
+
+Run with 
+````bash
+> npx serve
+````
---
+++ src/app.js
@@ -0,0 +1,72 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            title: 'Ms',
+            firstName: 'Berkan',
+            lastName: 'Kütük',
+            email: 'berkan@kutuk.dev',
+            gender: 'male',
+            age: '21',
+            nat: 'DK',
+            picture: '../assets/berkan.png',
+
+            cell: '31416922',
+            phone: '31416922',
+
+            city: 'Odense',
+            country: 'Denmark',
+            postcode: '5230',
+            state: 'Syddanmark',
+            street: 'Cool street',
+            streetNo: '1337',
+
+            username: 'Berkanktk',
+            password: 'helloworld',
+            uuid: '5d4awd4q184da2w56d8q6w',
+            pw_md5: 'fc5e038d38a57032085441e7fe7010b0',
+            pw_sha256: '936a185caaa266bb9cbe981e9e05cb78cd732b0b3280eb944412bb6f8f8f07af',
+
+            reg_age: '21',
+            reg_date: '2018-10-31T21:29:59.315'
+
+        }
+    },
+    methods: {
+        async getUser() {
+            const res = await fetch('https://randomuser.me/api/')
+            const { results } = await res.json()
+
+            // console.log(results)
+
+            this.title = results[0].name.title
+            this.firstName = results[0].name.first
+            this.lastName = results[0].name.last
+            this.email = results[0].email
+            this.gender = results[0].gender
+            this.age = results[0].dob.age
+            this.nat = results[0].nat
+            this.picture = results[0].picture.large
+
+            this.cell = results[0].cell
+            this.phone = results[0].phone
+
+            this.city = results[0].location.city
+            this.country = results[0].location.country
+            this.postcode = results[0].location.postcode
+            this.state = results[0].location.state
+            this.street = results[0].location.street.name
+            this.streetNo = results[0].location.street.number
+
+            this.username = results[0].login.username
+            this.password = results[0].login.password
+            this.uuid = results[0].login.uuid
+            this.pw_md5 = results[0].login.md5
+            this.pw_sha256 = results[0].login.sha256
+
+            this.reg_age = results[0].registered.age
+            this.reg_date = results[0].registered.date
+
+        },
+    },
+})
+app.mount('#app')
